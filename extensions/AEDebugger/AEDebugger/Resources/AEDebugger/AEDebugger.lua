223b94e06d0cf610b364af03f37183767f7fb676
David Wimsey <david@wimsey.us>
2014-02-14 05:02:14 +0000

    More debugger ground work
    git-svn-id: http://svn.wimsey.us/development/AmbienceEngineSDK/trunk@27 ee46bd55-b9af-4c50-9253-ad5f3371387c

--- extensions/AEDebugger/AEDebugger/Resources/AEDebugger/AEDebugger.lua
+++ extensions/AEDebugger/AEDebugger/Resources/AEDebugger/AEDebugger.lua
@@ -1,19 +1,44 @@
 function DebuggerInit(forceDebuggerEnabled)
 	local prefs = AEEngine:GetPreferencesManager()
 
-	if forceDebuggerEnabled then
-		prefs:SetBoolean('AEDebugger/enabled', true)
-	end
+    local isVerboseDebug = false
+    if prefs:GetBoolean('AEDebugger/verbose', false) == true then
+        isVerboseDebug = true
+        print "AEDebugger: VERBOSE"
+    end
 
-	if prefs:GetBoolean('AEDebugger/enabled', false) == false then
+    if forceDebuggerEnabled then
+        if isVerboseDebug then
+            print "AEDebugger: Forcably ENABLED."
+        end
+        prefs:SetBoolean('AEDebugger/enabled', true)
+    else
+        if forceDebuggerDisabled then
+            if isVerboseDebug then
+                print "AEDebugger: Forcably DISABLED."
+            end
+            prefs:SetBoolean('AEDebugger/enabled', false)
+        end
+    end
+
+    if prefs:GetBoolean('AEDebugger/enabled', false) == false then
+        if isVerboseDebug then
+            print "AEDebugger: Disabled, not attaching."
+        end
 		return
 	end
+    if isVerboseDebug then
+        print "AEDebugger: Attaching ..."
+    end
 
 	AEDebuggerRocketContext = rocket.contexts["AEViewportContext"]
 	if AEDebuggerRocketContext == nil then
 		AEError("Could not find default viewport for AEDebugger librocket context.");
 		return
 	end
+    if isVerboseDebug then
+        print("AEDebugger: GUI context:", AEDebuggerRocketContext)
+    end
 
 	local dConsoleURL = "FILE://AEDebugger/GUI/console.rml"
 	AEDebuggerRocketConsole = AEDebuggerRocketContext:LoadDocument(dConsoleURL)
@@ -21,10 +46,16 @@ function DebuggerInit(forceDebuggerEnabled)
 		AEError("Could not load debugger console: " .. dConsoleURL);
 		return
 	end
+    if isVerboseDebug then
+        print("AEDebugger: Console window:", AEDebuggerRocketConsole)
+    end
 	AEDebuggerRocketConsole:Show()
 
-	print "AEDebugger attached2."
-	self = nil
+    if isVerboseDebug then
+        print "AEDebugger: Attached."
+    end
+
+    self = nil
 end
 
 DebuggerInit()
